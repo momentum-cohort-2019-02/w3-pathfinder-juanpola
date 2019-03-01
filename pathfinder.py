c6efd7bf081a897a94d51fe806551478ea3f8071
Juan polanco <juanpolanco5008@gmail.com>
2019-03-01 14:39:17 -0500

    created classes that i think that i will be working with!

---
+++ pathfinder.py
@@ -0,0 +1,19 @@
+"""Reads map data from the list"""
+class MapPicture:
+    pass
+
+
+"""Draws colored pixels using map data"""
+class Draw:
+    pass
+   
+
+
+
+
+if __name__ == "__main__":
+    pass
+
+
+
+
