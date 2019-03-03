64213c5110ab68f94c30a9a1fa4a70cadb2f65e4
Juan polanco <juanpolanco5008@gmail.com>
2019-03-03 11:10:28 -0500

    added map class and with open file

--- pathfinder.py
+++ pathfinder.py
@@ -1,30 +1,31 @@
 import random
 from PIL import ImageColor, Image, ImageDraw
 
-with open('elevation_small.txt') as f:
-    lines = f.read().split()
+class Map:
+    """
+    the class map Represents a map
+    """
 
-print(lines)
-
-# class MapPicture:
-#     """Reads map data from the list"""
-
-
-
-#     pass
-
-
-# class Draw:
-#     """Draws colored pixels using map data"""
-#     pass
-   
-
-
-
-
-# if __name__ == "__main__":
-#     pass
-
-
-
+    def __init__(self, file, map_drawer, path_finder, ):
+        """
+        Parameters:
+        - file -- elevation data file to be read in
+        - map_drawer -- represents MapDrawer object
+        - path_finder -- represents PathFinder object
+        """
+        self.file = elevation_small.txt
+        self.map_drawer = map_drawer
+        self.path_finder = path_finder
+        self.y_coord = y_coord
+        self.elevations = self.convert_to_list()
+        
+    def convert_to_list(self):
+        """
+        the self.elevation_small.txt will be turned into a list. 
+        """
+        elevations = []
+        with open(self.elevation_small.txt) as file:
+            for line in file:
+                elevations.append([int(e) for e in line.split(" ")])
+        return elevations
 
